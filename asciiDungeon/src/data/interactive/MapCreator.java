d96a874d949ad02cc48db9d17784a6a18df0022a
Trhe_piernas <hruizdeegino@gmail.com>
2024-04-24 23:32:00 +0200

    fix erros

--- asciiDungeon/src/data/interactive/MapCreator.java
+++ asciiDungeon/src/data/interactive/MapCreator.java
@@ -4,8 +4,7 @@ import data.GameObject;
 import data.Vector2;
 import data.exceptions.GameLogicException;
 import data.noInteractive.Estatikoa;
-import data.noInteractive.Formak;
-import kalkuloak.GameMain;
+import data.noInteractive.Formak;import kalkuloak.GameMain;
 import kalkuloak.MapCreatorMain;
 import render.GraficsConfig;
 
@@ -18,15 +17,10 @@ public class MapCreator extends GameObject {
     private char azkenZapaldutakoTekla;
 
     private GameObject objetua = new Estatikoa(Formak.FLOOR);
-    private Boolean intereactive = false;
     private static MapCreatorMain gameMain = MapCreatorMain.getMapCreatorData();
 
     private MapCreator(Formak forma, Vector2 posizioa) {
         super(forma, posizioa);
-    }
-
-    public Boolean getIntereactive() {
-        return intereactive;
     }
 
     /**
@@ -65,13 +59,6 @@ public class MapCreator extends GameObject {
         if (x > GraficsConfig.GAME_X_GRID_SIZE || y > GraficsConfig.GAME_Y_GRID_SIZE) {
             return gameMain.getMap().getMatrix();
         }
-        if (intereactive) {
-            return updateInteractive(x, y);
-        }
-        return updateBackground(x, y);
-    }
-
-    public GameObject[][] updateBackground(int x, int y){
         GameObject[][] matrizea = gameMain.getMap().getMatrix();
         try {
             matrizea[getX()][getY()] = objetua;
@@ -80,19 +67,6 @@ public class MapCreator extends GameObject {
             System.out.println("Matrizearen limetean zaude posizio honeatik ezin gara joan");
         }
 
-        matrizea[this.getX()][this.getY()] = this;
-        return matrizea;
-    }
-
-    public GameObject[][] updateInteractive(int x, int y){
-        GameObject[][] matrizea = gameMain.getMap().getMatrix();
-
-        try {
-            matrizea[getX()][getY()] = objetua;
-            setPosizioa(new Vector2(getX() + x, getY() + y));
-        } catch (GameLogicException e) {
-            System.out.println("Matrizearen limetean zaude posizio honeatik ezin gara joan");
-        }
         matrizea[this.getX()][this.getY()] = this;
         return matrizea;
     }
@@ -115,25 +89,16 @@ public class MapCreator extends GameObject {
             matrizea = mugitu(1, 0);
         } else if (azkenZapaldutakoTekla == '1') {
             objetua = new Estatikoa(Formak.WALL);
-            intereactive = false;
         } else if (azkenZapaldutakoTekla == '2') {
             objetua = new Estatikoa(Formak.FLOOR);
-            intereactive = false;
         } else if (azkenZapaldutakoTekla == '3') {
             objetua = new Estatikoa(Formak.ENEMY);
-            intereactive = true;
         } else if (azkenZapaldutakoTekla == '5') {
             objetua = new Estatikoa(Formak.DOOR);
-            intereactive = true;
         } else if (azkenZapaldutakoTekla == '6'){
             objetua = new Estatikoa(Formak.KEY);
-            intereactive = true;
         } else if (azkenZapaldutakoTekla == '+'){
             objetua = null;
-            intereactive = true;
-        } else if (azkenZapaldutakoTekla == '-') {
-            objetua = null;
-            intereactive = false;
         }
         azkenZapaldutakoTekla = ' ';
         return matrizea;
